18415b80f1840a902e9a6e7de238b496ba221f20
baggio4ever <baggio4ever@gmail.com>
2024-02-15 22:06:24 +0900

    まだまだ勉強中

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+'use client'
 import Link from 'next/link'
+import { Roboto, Noto_Sans } from 'next/font/google';
+import { useSearchParams } from 'next/navigation';
+
+
+const Roboto900 = Roboto({ weight: '900', preload: false });
+const NotoSans = Noto_Sans({ weight: '400', preload: false });
+
+const Gozita = '/images/GFAADCqboAAf2_T.png';
+const BlueCombi = '/images/FKVYrTbakAE-gys.png';
+const Super3 = '/images/GBV7TPQa4AABls6.png';
 
 export default function Home() {
+  const searchParams = useSearchParams();
+  const val=searchParams.get('val');
   return (
     <div className=" text-white">
       <div className="font-bold text-5xl p-2">
@@ -14,6 +27,21 @@ export default function Home() {
           <Link href="about">about (by Link)</Link>
         </li>
       </ul>
+      <div className={` text-4xl ${Roboto900.className}`}>
+        Google Roboto 900
+      </div>
+      <div className={`text-4xl ${NotoSans.className}`}>
+        Google Noto Sans 400
+      </div>
+
+      <div className=' flex items-center border-red-500 border-2'>
+        <img src={Gozita} className=' w-48 m-5' title='ゴジータ！'></img>
+        <img src={BlueCombi} className=' w-64 m-5'></img>
+      </div>
+
+      <div className='border-2 border-green-400'>
+        {val}
+      </div>
     </div>
   );
 }
