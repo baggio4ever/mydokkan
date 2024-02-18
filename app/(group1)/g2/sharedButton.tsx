1de62a363021a569088f67af2b1490de3336f380
baggio4ever <baggio4ever@gmail.com>
2024-02-18 22:09:51 +0900

    眠い。

--- app/(group1)/g2/sharedButton.tsx
+++ app/(group1)/g2/sharedButton.tsx
@@ -1,4 +1,10 @@
-export default function SharedButton({ caption, count, onClick }) {
+interface SharedButtonProps {
+    caption: string;
+    count: number;
+    onClick: () => void;
+}
+
+export default function SharedButton({ caption, count, onClick }: SharedButtonProps) {
     return <div>
         <button className=" px-8 py-2 my-1 bg-gray-700 rounded font-mono" onClick={onClick}>{`${caption}:${count}`}</button>
     </div>
