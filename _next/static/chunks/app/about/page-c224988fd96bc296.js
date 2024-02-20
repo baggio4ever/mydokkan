2e15542c824d01943c2142752c4a8a4cb4c8e0e3
baggio4ever <baggio4ever@gmail.com>
2024-02-20 22:27:22 +0900

    Updates

---
+++ _next/static/chunks/app/about/page-c224988fd96bc296.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301],{5271:function(t,n,e){Promise.resolve().then(e.t.bind(e,638,23))},638:function(t){t.exports={tt:"about_tt__XtEkW"}}},function(t){t.O(0,[971,69,744],function(){return t(t.s=5271)}),_N_E=t.O()}]);
